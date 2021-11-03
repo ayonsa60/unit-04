3bc5f28fd76a55e7f0a16c77b4107d8c07e3e508
ayonsa60 <ayonsa60@gmail.com>
2021-11-03 00:21:30 -0500

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,64 @@
+// make a timed quiz that stores high scores
+// when the start button is clicked the timer starts and i am presented with a question
+// when i answer a question the next question loads
+// when a question is answered incorrectly time is subtracted from the clock
+// when all the questions are answered the timer reaches zero then the game is over
+// i can then save my initials and my score
+
+var timer;
+
+function startTime() {
+    timer = setInterval(startTime, 300000);
+
+
+    document.getElementById('bttn').innerHTML
+}
+document.getElementById('bttn').addEventListener('click', startTime);
+
+
+var question = []; {
+    question[0] = "Which of the following is a correct variable value?";
+    question[1] = "What is an array?";
+    question[2] = "Which of the following is the correct wat to name a variable?";
+    question[3] = "What is the right way to write an array?";
+    question[4] = "Which operator is used to assign a value to a variable?";
+    }
+
+
+
+
+// Which of the following is a correct variable value?
+// Boolean
+// "String"
+// Number
+// All of the above
+
+// What is an array?
+// A group of values that are stored in a particular order
+// data value that can change depending on conditions or information passed to the program
+// A function that displays a defined variable
+// A standalone entity, with properties and type
+
+// Which of the following is the correct wat to name a variable?
+// Hyphen
+// CamelCase
+// Parentheses
+// Commas
+
+// What is the right way to write an array?
+// var = {}
+// var = ""
+// var = ()
+// var = []
+
+// Which operator is used to assign a value to a variable?
+// =
+// :
+// ()
+// *
+
+// // <!-- <script> -->
+// document.getElementById("bttn").addEventListener("click", function() {
+//     document.getElementById("demo").innerHTML = -->
+//   });
+//   </script>
