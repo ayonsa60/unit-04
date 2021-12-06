f5e9be074e845f1a796c1918cbd998b908e542b1
ayonsa60 <ayonsa60@gmail.com>
2021-12-05 22:34:16 -0600

    added variables

--- assets/script.js
+++ assets/script.js
@@ -5,62 +5,67 @@
 // when all the questions are answered the timer reaches zero then the game is over
 // i can then save my initials and my score
 
-var time = startTime;
+// DOM variables
+var homePage = document.getElementById("home-page");
+var quizPage = document.getElementById("quiz-page");
+var lastPage = document.getElementById("last-page");
 
-function startTime() {
+var quizQuestions = [
+    {
+        question: "Which of the following is a correct variable value?",
+        options: [ "Boolean", "String", "Number", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "What is an array?",
+        options: ["A group of values that are stored in a particular order",
+                "data value that can change depending on conditions or information passed to the program",
+                "A function that displays a defined variable",
+                "A standalone entity, with properties and type"],
+        answer: "A group of values that are stored in a particular order"
+    },
+    {
+        question: "Which of the following is the correct way to name a variable?",
+        options: ["Hyphen", "CamelCase", "Parentheses", " Commas"],
+        answer: "CamelCase"
+    },
+    {
+        question:  "What is the right way to create an array?",
+        options: ["{}", "''", "()", "[]"],
+        answer: "[]"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        options: [ "=", ":", "()", ";"],
+        answer: "="
+    }
+]
+function startQuiz() {
+    var startQuiz = document.getElementById('quiz-page');
+    startQuiz.setAttribute("class", "hide");
+
+    // timer code block
     time = setInterval(startTime, 300000);
-
-
-    var countDownEl = document.getElementById('countDown');
-}
-document.getElementById('bttn').addEventListener('click', startTime);
-
-
-var questions = []; {
-    question[0] = "Which of the following is a correct variable value?";
-    question[1] = "What is an array?";
-    question[2] = "Which of the following is the correct wat to name a variable?";
-    question[3] = "What is the right way to write an array?";
-    question[4] = "Which operator is used to assign a value to a variable?";
-}
-for( i = 0; i < questions.length; i++) {
-
 }
 
 
 
-// Which of the following is a correct variable value?
-// Boolean
-// "String"
-// Number
-// All of the above
 
-// What is an array?
-// A group of values that are stored in a particular order
-// data value that can change depending on conditions or information passed to the program
-// A function that displays a defined variable
-// A standalone entity, with properties and type
 
-// Which of the following is the correct wat to name a variable?
-// Hyphen
-// CamelCase
-// Parentheses
-// Commas
+//     var countDownEl = document.getElementById('countDown');
+// }
+// document.getElementById('bttn').addEventListener('click', startTime);
 
-// What is the right way to write an array?
-// var = {}
-// var = ""
-// var = ()
-// var = []
 
-// Which operator is used to assign a value to a variable?
-// =
-// :
-// ()
-// *
+
+// for( i = 0; i < questions.length; i++) {
+
+// }
 
 // // <!-- <script> -->
 // document.getElementById("bttn").addEventListener("click", function() {
 //     document.getElementById("demo").innerHTML = -->
 //   });
 //   </script>
+// function startTime() {
+    // time = setInterval(startTime, 300000);
